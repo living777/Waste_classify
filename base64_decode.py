37db6c39ea298a446d5efd18dd4ebcb49dc8a4d2
living777 <863759693@qq.com>
2022-06-30 09:05:39 +0800

    Flask 框架+前端

---
+++ base64_decode.py
@@ -0,0 +1,48 @@
+import base64
+import re
+import uuid
+
+
+def decode_image(src):
+    """
+    解码图片
+    :param src: 图片编码
+        eg:
+            src="data:image/gif;base64,R0lGODlhMwAxAIAAAAAAAP///
+                yH5BAAAAAAALAAAAAAzADEAAAK8jI+pBr0PowytzotTtbm/DTqQ6C3hGX
+                ElcraA9jIr66ozVpM3nseUvYP1UEHF0FUUHkNJxhLZfEJNvol06tzwrgd
+                LbXsFZYmSMPnHLB+zNJFbq15+SOf50+6rG7lKOjwV1ibGdhHYRVYVJ9Wn
+                k2HWtLdIWMSH9lfyODZoZTb4xdnpxQSEF9oyOWIqp6gaI9pI1Qo7BijbF
+                ZkoaAtEeiiLeKn72xM7vMZofJy8zJys2UxsCT3kO229LH1tXAAAOw=="
+
+    :return: str 保存到本地的文件名
+    """
+    # 1、信息提取
+    result = re.search("data:image/(?P<ext>.*?);base64,(?P<data>.*)", src, re.DOTALL)
+    if result:
+        ext = result.groupdict().get("ext")
+        data = result.groupdict().get("data")
+
+    else:
+        raise Exception("Do not parse!")
+
+    # 2、base64解码
+    img = base64.urlsafe_b64decode(data)
+
+    # 3、二进制文件保存
+    filename = "{}.{}".format(uuid.uuid4(), ext)
+    with open(filename, "wb") as f:
+        f.write(img)
+
+    return filename
+
+
+if __name__ == '__main__':
+
+    src=""
+
+    binary=base64.b64decode(src)
+    f=open('D:\\computer Design','wb')
+    f.write(binary)
+    f.close()
+    # print(encode_image('example.jpg'))
